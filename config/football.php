a702dc6e8159be58ed4ae583556d341320c3a1cd
Serhii Matsenko <matsenko2502@gmail.com>
2022-03-17 12:12:17 +0200

    add football config

---
+++ config/football.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'action' => [
+        'win' => 0,
+        'drawn' => 2,
+        'lost' => 2
+    ],
+
+    'point' => [
+        'win' => 3,
+        'drawn' => 1,
+        'lost' => 0
+    ],
+
+    'goal_difference' => [
+        'top_rate' => 1.5,
+        'bottom_rate' => 0.5,
+        'additional_percent' => 5
+    ]
+];
